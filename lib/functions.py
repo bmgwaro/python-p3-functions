e35194e24353e8f4abb1a0aec4a8025d3a791337
bmgwaro <brian.gwaro@student.moringaschool.com>
2024-08-27 15:53:28 +0300

    functions

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python3
 
-def greet_programmer():
-    pass
-
-def greet(name):
-    pass
-
-def greet_with_default(name="programmer"):
-    pass
-
-def add(num1, num2):
-    pass
-
-def halve(number):
-    pass
+def greet_programmer(): 
+    print("Hello, programmer!") 
+    
+def greet(name): 
+    print(f"Hello, {name}!") 
+    
+def greet_with_default(name="programmer"): 
+    print(f"Hello, {name}!") 
+    
+def add(num1, num2): 
+    return num1+num2 
+    
+def halve(number): 
+    return number/2
